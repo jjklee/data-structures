4e4f044d5dd246f5e4016190d079f387b38134fc
jjklee <jjlee720@gmail.com>
2019-05-29 14:03:39 -0700

    Passes all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  
+  var storageSize = Object.keys(storage).length;
+  
   // Implement the methods below
   someInstance.push = function(value) {
+      // console.log(i, storageSize, storage[i]);
+        storage[storageSize] = value;
+        console.log(storageSize, storage);
+        storageSize += 1;
   };
 
   someInstance.pop = function() {
+    if (storageSize > 0) {
+      let last = storage[storageSize-1];
+      delete(storage[storageSize - 1]);
+      storageSize -= 1;
+      return last;
+    }
   };
 
   someInstance.size = function() {
+    return storageSize
   };
 
   return someInstance;
 };
+
+var stack = Stack();
