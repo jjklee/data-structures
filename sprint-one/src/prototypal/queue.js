83e530d322d0b0d7e3161d9ea67a4132c430698f
jjklee <jjlee720@gmail.com>
2019-05-29 21:29:20 -0700

    Passess tests for the prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.storageSize = 0;
+  newQueue.currentQueue = 0;
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.storageSize + this.currentQueue] = value;
+  this.storageSize += 1;
+};
+
+queueMethods.dequeue = function() {
+  if (this.storageSize > 0) {
+    let first = Object.values(this.storage)[0];
+    delete this.storage[Object.keys(this.storage)[0]];
+    this.currentQueue += 1;
+    this.storageSize -= 1;
+    return first;
+  }
+};
+
+queueMethods.size = function() {
+  return this.storageSize;
+};
 
