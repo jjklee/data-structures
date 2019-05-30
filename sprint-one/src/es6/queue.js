0ac34419643e5fe4d90f9fa74b2df67e4cbacf87
jjklee <jjlee720@gmail.com>
2019-05-30 11:16:04 -0700

    Completed es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storageSize = 0;
+    this.currentQueue = 0;
+  }
+  
+  enqueue(value) {
+    this.storage[this.storageSize + this.currentQueue] = value;
+    this.storageSize++;
   }
 
+  dequeue() {
+    let first = Object.values(this.storage)[0];
+    if (this.storageSize > 0) {
+      delete this.storage[Object.keys(this.storage)[0]];
+      this.currentQueue++;
+      this.storageSize--;
+      return first;
+    }
+  
+  }
+
+  size() {
+    return this.storageSize;
+  }
 }
+
+var newQueue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storageSize = 0;
+  }
+  
+  push(value) {
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
   }
 
+  pop() {
+    let last = this.storage[this.storageSize - 1];
+    if (this.storageSize > 0) {
+      delete this.storage[this.storageSize - 1];
+      this.storageSize--;
+    }
+    return last;
+  }
+
+  size () {
+    return this.storageSize;
+  }
 }
+
+var stack = new Stack();
