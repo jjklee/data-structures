e88e62dd1e3c185977c26ad1bac516896f71e9ef
jjklee <jjlee720@gmail.com>
2019-05-30 19:15:19 -0700

    First commit

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,38 @@
+// Code goes here
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    let newNode = new Node(value);
+    let current;
+    if (list.head === null) {
+      list.head = newNode;
+    } else if (list.head !== null) {
+      current = list.head;
+      current.next = newNode;
+    }
+    list.tail = newNode;
   };
-
+  
   list.removeHead = function() {
+    if (list.head !== null) {
+      list.head = list.head.next;
+      let currentHead = list.head;
+      delete list.head;
+      return currentHead;
+    }
   };
 
   list.contains = function(target) {
+    // for (let key in target) {
+    //   if (Object.values(target).includes(target)) {
+    //     return true;
+    //   }
+    //   return false;
+    // }
   };
 
   return list;
@@ -24,6 +47,7 @@ var Node = function(value) {
   return node;
 };
 
+linkedList = LinkedList();
 /*
  * Complexity: What is the time complexity of the above functions?
  */
