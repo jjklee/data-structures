bd1706798e5671bd3ec016c24ee92b5f8ff44455
jjklee <jjlee720@gmail.com>
2019-05-30 23:18:20 -0700

    Completes tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +12,26 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
+
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length !== 0) {
+    let arr = this.children.map(child => {
+      return child.contains(target);
+    }); 
+    if (arr.indexOf(true) > -1) {
+      return true;
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild - constant
+ contains - exponential
  */
