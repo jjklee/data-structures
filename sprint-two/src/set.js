018d8e3817fa993f825de0d3af57060d4e379dc5
jjklee <jjlee720@gmail.com>
2019-05-31 11:20:33 -0700

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.includes(item)) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  let index = this._storage.indexOf(item);
+  if (index > -1) {
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
