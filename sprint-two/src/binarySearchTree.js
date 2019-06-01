5a9422642e9fa4f539ff346c1b4aef355e3f81b3
jjklee <jjlee720@gmail.com>
2019-06-01 12:27:34 -0700

    Completes binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,62 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function (value) {
+  var newTree = new BinarySearchTree(value);
+  if (value > this.value) {
+    if (this.right !== null) {
+      this.right.insert(value);
+    } else {
+      this.right = newTree;
+    }
+  } else {
+    if (this.left !== null) {
+      this.left.insert(value);
+    } else {
+      this.left = newTree;
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+  var condition = false;
+  
+  var helper = function(obj) {
+    if (obj === null) {
+      return;
+    }
+    if (obj.value === value) {
+      condition = true;
+    }
+    helper(obj.right);
+    helper(obj.left);
+    
+  };
+  
+  helper(this);
+  return condition;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+  var helper = function(obj) {
+    cb(obj.value);
+    if (obj.left !== null) {
+      helper(obj.left);
+    }
+    if (obj.right !== null) {
+      helper(obj.right);
+    } 
+  };
+  helper(this);
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert - log
+ contains - log
+ depthFirstLog - linear
  */
