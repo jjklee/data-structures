a44f94088c0000e3fa0b950bcde8fa06fd01ba0d
jjklee <jjlee720@gmail.com>
2019-06-07 11:13:47 -0700

    First attempt at resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,27 +1,34 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage[index] === undefined) {
+    this.size++;
     this._storage[index] = [[k, v]];
   } else {
-    for (let i = 0; i < this._storage[index].length; i ++) {
+    for (let i = 0; i < this._storage[index].length; i++) {
       if (this._storage[index][i][0] === k) {
         this._storage[index][i][1] = v;
-      } else {
-        this._storage[index].push([k, v]);
-      }
+      } 
     }
+    this.size++;
+    this._storage[index].push([k, v]);
+  }
+
+  if (this.size / this._limit > 0.75) {
+    this.resize(this._limit * 2);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage[index]) {
+    return;
+  }
   for (let i = 0; i < this._storage[index].length; i ++) {
     if (this._storage[index][i][0] === k) {
       return this._storage[index][i][1];
@@ -33,7 +40,31 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   for (let i = 0; i < this._storage[index].length; i ++) {
     if (this._storage[index][i][0] === k) {
+      this.size--;
       delete this._storage[index][i][1];
+    }
+  }
+
+  if (this.size / this._limit < 0.25) {
+    this.resize(this._limit / 2);
+  }
+};
+
+HashTable.prototype.resize = function(limit) {
+  var oldStorage = this._storage;
+  this._limit = limit;
+  this._storage = LimitedArray(limit);
+
+  for (let i = 0; i < oldStorage.length; i++) {
+    if (oldStorage[i]) {
+      for (let j = 0; j < oldStorage[i].length; j++) {
+        var index = getIndexBelowMaxForKey(oldStorage[i][j], limit);
+        if (this._storage[index]) {
+          this._storage[index].push([oldStorage[i][j][0], oldStorage[i][j][1]]);
+        } else {
+          this._storage[index] = [[oldStorage[i][j][0], oldStorage[i][j][1]]];
+        }
+      }
     }
   }
 };
@@ -41,6 +72,7 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  linear
+ resize - quadratic
  */
 
 
